be1ce6d893b226be42cabc993d5559f9720277df
구본철 <bbon@bbon.kr>
2020-01-01 22:41:02 +0900

    프로젝트 추가

---
+++ App.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform,
+  NativeSyntheticEvent,
+  TextInputChangeEventData
+} from "react-native";
+
+const { width } = Dimensions.get("window");
+
+const App = () => {
+  const [newToDo, setNewToDo] = useState("");
+
+  const handleChangeInput = (text: string) => {
+    setNewToDo(text);
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>I want to do</Text>
+      <View style={styles.card}>
+        <TextInput
+          style={styles.input}
+          placeholder="New to do"
+          placeholderTextColor="#ccc"
+          value={newToDo}
+          onChangeText={handleChangeInput}
+          returnKeyLabel="Done"
+          returnKeyType="done"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f96080",
+    alignItems: "center"
+    // justifyContent: "center"
+  },
+  title: {
+    color: "#fff",
+    fontSize: 40,
+    fontWeight: "600",
+    textTransform: "uppercase",
+    marginTop: 60,
+    marginBottom: 40
+  },
+  card: {
+    flex: 1,
+    backgroundColor: "#fff",
+    width: width - 40,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+        shadowOffset: {
+          height: -3,
+          width: 2
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {
+    padding: 14,
+    fontSize: 30,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1
+  }
+});
+
+export default App;
